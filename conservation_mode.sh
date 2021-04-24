76b60770045f8560047e5aeaead647917f24e7f6
Emil Suleymanov <emil.suleymanov@tum.de>
2021-04-24 18:34:32 +0200

    Cleanup and add raw get state opt

--- conservation_mode.sh
+++ conservation_mode.sh
@@ -22,16 +22,19 @@ set -e
 
 MODULE_NAME="ideapad_laptop"
 PROMPT="$MODULE_NAME is not loaded! Do you want to try loading it? [Y/n] "
+CONTROL_PATH="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
 
-ENABLE=0
-DISABLE=0
-ASSUME_YES=0
+OPT_ENABLE=0
+OPT_DISABLE=0
+OPT_ASSUME_YES=0
+OPT_GET_STATE=0
+OPT_RAW_GET_STATE=0
 
 mod_check() {
     if lsmod | grep "$MODULE_NAME" &>/dev/null; then
         echo "$MODULE_NAME module is already loaded."
     else
-        if [ "$ASSUME_YES" == "1" ]; then
+        if [ "$OPT_ASSUME_YES" == "1" ]; then
             echo "${PROMPT}yes"
             response="yes"
         else
@@ -59,21 +62,29 @@ print_usage() {
     echo " -d, --disable               disable the \"Battery Conservation Mode\""
     echo " -e, --enable                enable the \"Battery Conservation Mode\""
     echo " -y                          assume \"yes\" to all questions"
+    echo " -g, --get-state             get current state of the \"Battery Conservation Mode\""
+    echo " -r, --raw-get-state         get current state of the \"Battery Conservation Mode\" in stdout as a number"
     echo " -h, --help                  display this help"
 }
 
 while test $# -gt 0; do
     case "$1" in
     --enable | -e)
-        ENABLE=1
+        OPT_ENABLE=1
         ;;
     --disable | -d)
-        DISABLE=1
+        OPT_DISABLE=1
         ;;
     -y)
-        ASSUME_YES=1
+        OPT_ASSUME_YES=1
         ;;
-    --help|-h)
+    --get-state | -g)
+        OPT_GET_STATE=1
+        ;;
+    --raw-get-state | -r)
+        OPT_RAW_GET_STATE=1
+        ;;
+    --help | -h)
         print_usage
         exit 0
         ;;
@@ -89,19 +100,32 @@ while test $# -gt 0; do
     shift
 done
 
-if [ "$ENABLE" == "1" ] && [ "$DISABLE" == "1" ]; then
+OPTIONS_VER_SUM=$((OPT_ENABLE + OPT_DISABLE + OPT_GET_STATE + OPT_RAW_GET_STATE))
+
+if [ "$OPTIONS_VER_SUM" -gt "1" ]; then
     print_usage
     exit 1
-elif [ "$ENABLE" == "1" ]; then
+elif [ "$OPT_ENABLE" == "1" ]; then
     mod_check
     # Turn on
-    echo 1 >/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+    echo 1 >$CONTROL_PATH
     echo "Conservation mode enabled."
-elif [ "$DISABLE" == "1" ]; then
+elif [ "$OPT_DISABLE" == "1" ]; then
     mod_check
     # Turn off
-    echo 0 >/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+    echo 0 >$CONTROL_PATH
     echo "Conservation mode disabled."
+elif [ "$OPT_GET_STATE" == "1" ]; then
+    STATE=$(cat $CONTROL_PATH)
+    if [ "$STATE" == "1" ]; then
+        echo "Conservation mode is enabled."
+    elif [ "$STATE" == "0" ]; then
+        echo "Conservation mode is disabled."
+    else
+        echo "Conservation mode is in an unknown state: $STATE."
+    fi
+elif [ "$OPT_RAW_GET_STATE" == "1" ]; then
+    cat $CONTROL_PATH
 else
     print_usage
     exit 1
