1afdc565f34c83d8c883e5d2cadb416e5df68570
Emil Suleymanov <emil.suleymanov@tum.de>
2021-04-24 18:36:34 +0200

    Remove double-spaces

--- conservation_mode.sh
+++ conservation_mode.sh
@@ -11,11 +11,11 @@
 #
 # "Lenovo Conservation Mode Helper" is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Lenovo Conservation Mode Helper".  If not, see
+# along with "Lenovo Conservation Mode Helper". If not, see
 # <https://www.gnu.org/licenses/>.
 
 set -e
