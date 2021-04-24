ad5522243891e2598cb4c7a58a869540249374b1
Emil Suleymanov <emil.suleymanov@tum.de>
2021-04-24 18:14:30 +0200

    Update name, add installer

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# lenovo_conservation_mode
-A tool to enable/disable the "Battery Conservation Mode" on Lenovo laptops.
+# Lenovo Conservation Mode Helper
+A tool to enable/disable the "Battery Conservation Mode" on Lenovo laptops. See "--help" for usage.
--- conservation_mode.sh
+++ conservation_mode.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# "conservation_mode.sh" is a tool to enable/disable the "Battery Conservation
-# Mode" on Lenovo laptops.
-#
 # Copyright (c) 2021 Emil Suleymanov <suleymanovemil8@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of "Lenovo Conservation Mode Helper".
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# "Lenovo Conservation Mode Helper" is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# "Lenovo Conservation Mode Helper" is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with "Lenovo Conservation Mode Helper".  If not, see
+# <https://www.gnu.org/licenses/>.
 
 set -e
 
